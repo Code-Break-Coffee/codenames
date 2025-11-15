08c925c13e7d915fe95c8456f117d8f1e5bea0e8
zakie khan <121384625+zakie2003@users.noreply.github.com>
2025-11-15 17:14:41 +0530

    game card emiting for clicking done

--- backend/controllers/operativesController.js
+++ backend/controllers/operativesController.js
@@ -1,8 +1,33 @@
-function handleSocketEvents(io, socket) {
-  socket.on('sendMessage', (data) => {
-    console.log('📩 Received message:', data);
+const Game = require("../model/Game.js");
 
-    io.emit('receiveCardClick', data);
+function handleSocketEvents(io, socket) {
+
+  socket.on("joinGame", (gameId) => {
+    socket.join(gameId);
+    console.log(`🔵 User ${socket.id} joined game ${gameId}`);
+  });
+
+  socket.on("revealCard", async ({ gameId, cardId }) => {
+    console.log(`🟥 Card revealed in game ${gameId}: ${cardId}`);
+    let game=await Game.findById(gameId);
+    const card = game.board[cardId];
+    const color=card.type;
+    const updated_score={
+      redScore:game.redScore,
+      blueScore:game.blueScore
+    }
+    if(color=="blue"){
+      updated_score.blueScore=updated_score.blueScore-1;
+    }
+    if(color=="red"){
+      updated_score.redScore=updated_score.redScore-1;
+    }
+    let result= await Game.findByIdAndUpdate(gameId,{ $set:updated_score});
+    io.to(gameId).emit("cardRevealed", { cardId,updated_score });
+  });
+
+  socket.on("sendMessage", (data) => {
+    io.emit("receiveCardClick", data);
   });
 }
 
